54ea4b36685af485d7421f11db5bf525e39b0aa1
wael.a.farah <wael.a.farah@gmail.com>
2026-01-12 12:39:00 -0800

    Add Probe Rate (GPU) block

---
+++ include/gnuradio/cuda/probe_rate.h
@@ -0,0 +1,67 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2025
+ *
+ * This file is part of GNU Radio
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#ifndef INCLUDED_CUDA_PROBE_RATE_H
+#define INCLUDED_CUDA_PROBE_RATE_H
+
+#include <gnuradio/cuda/api.h>
+#include <gnuradio/sync_block.h>
+#include <string_view>
+
+namespace gr {
+namespace cuda {
+
+/*!
+ * \brief GPU-native throughput measurement block
+ * \ingroup cuda
+ *
+ * Measures throughput of GPU data streams without D2H transfer overhead.
+ * Operates directly on GPU buffers and reports sample rate via message port.
+ */
+class CUDA_API probe_rate : virtual public gr::sync_block {
+public:
+  typedef std::shared_ptr<probe_rate> sptr;
+
+  /*!
+   * \brief Make a GPU throughput measurement block
+   * \param itemsize size of each stream item
+   * \param update_rate_ms minimum update time in milliseconds
+   * \param alpha gain for running average filter
+   * \param name name for this probe (used in generated dictionaries)
+   *
+   * If the name is empty, the "name" field in the emitted dictionaries is
+   * omitted.
+   */
+  static sptr make(size_t itemsize, double update_rate_ms = 500.0,
+                   double alpha = 0.0001, std::string_view name = "");
+
+  /*!
+   * \brief Set the decay of the exponential rate average
+   */
+  virtual void set_alpha(double alpha) = 0;
+
+  /*!
+   * \brief Set the name of this probe
+   * Used in the emitted dictionaries if not empty
+   */
+  virtual void set_name(std::string_view name) = 0;
+
+  virtual double rate() = 0;
+
+  bool start() override = 0;
+  bool stop() override = 0;
+};
+
+} // namespace cuda
+} // namespace gr
+
+#endif /* INCLUDED_CUDA_PROBE_RATE_H */
+
+
--- python/cuda/bindings/python_bindings.cc
+++ python/cuda/bindings/python_bindings.cc
@@ -28,6 +28,7 @@ void bind_copy(py::module &m);
     void bind_cuda_helpers(py::module& m);
     void bind_null_source(py::module& m);
     void bind_null_sink(py::module& m);
+    void bind_probe_rate(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -71,5 +72,6 @@ PYBIND11_MODULE(cuda_python, m)
     bind_cuda_helpers(m);
     bind_null_source(m);
     bind_null_sink(m);
+    bind_probe_rate(m);
     // ) END BINDING_FUNCTION_CALLS
 }
