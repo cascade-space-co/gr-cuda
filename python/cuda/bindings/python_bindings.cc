c2f9db0e6401aae04ab5142ee7176d5e350cefe8
wael.a.farah <wael.a.farah@gmail.com>
2026-03-27 14:13:11 -0700

    Make IBV blocks optional (auto-detect libibverbs)

--- python/cuda/bindings/python_bindings.cc
+++ python/cuda/bindings/python_bindings.cc
@@ -39,8 +39,10 @@ void bind_null_sink(py::module& m);
 void bind_probe_rate(py::module& m);
 void bind_throttle(py::module& m);
 void bind_tee(py::module& m);
+#ifdef ENABLE_IBV
 void bind_ibv_source(py::module& m);
 void bind_ibv_sink(py::module& m);
+#endif
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -94,7 +96,9 @@ PYBIND11_MODULE(cuda_python, m)
     bind_probe_rate(m);
     bind_throttle(m);
     bind_tee(m);
+#ifdef ENABLE_IBV
     bind_ibv_source(m);
     bind_ibv_sink(m);
+#endif
     // ) END BINDING_FUNCTION_CALLS
 }
